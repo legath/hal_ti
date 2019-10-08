360a1b63d9aabe76ca87fe4fed356d6c51cefb2d
Stephanos Ioannidis <root@stephanos.io>
2019-10-08 16:01:33 +0900

    hercules: Add initial structure for Hercules HAL.
    Construct base framework for TI Hercules HAL and add register headers.
    
    Signed-off-by: Stephanos Ioannidis <root@stephanos.io>

---
+++ hercules/hal/hercules_hal.h
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2019 Stephanos Ioannidis <root@stephanos.io>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef HERCULES_HAL_H_
+#define HERCULES_HAL_H_
+
+/* Include register definition headers */
+#include "include/reg_adc.h"
+#include "include/reg_can.h"
+#include "include/reg_crc.h"
+#include "include/reg_dcc.h"
+#include "include/reg_dma.h"
+#include "include/reg_ecap.h"
+#include "include/reg_efc.h"
+#include "include/reg_eqep.h"
+#include "include/reg_esm.h"
+#include "include/reg_etpwm.h"
+#include "include/reg_flash.h"
+#include "include/reg_gio.h"
+#include "include/reg_het.h"
+#include "include/reg_htu.h"
+#include "include/reg_i2c.h"
+#include "include/reg_lin.h"
+#include "include/reg_mibspi.h"
+#include "include/reg_pbist.h"
+#include "include/reg_pcr.h"
+#include "include/reg_pinmux.h"
+#include "include/reg_pmm.h"
+#include "include/reg_pom.h"
+#include "include/reg_rti.h"
+#include "include/reg_sci.h"
+#include "include/reg_spi.h"
+#include "include/reg_stc.h"
+#include "include/reg_system.h"
+#include "include/reg_tcram.h"
+#include "include/reg_vim.h"
+
+#endif /* HERCULES_HAL_H_ */
